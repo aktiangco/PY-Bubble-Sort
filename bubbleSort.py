ef33a5be19dc56df1d83f5adc140bb07bfce63a0
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-05-15 19:12:51 -0700

    Random number

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,4 +1,6 @@
 # write your bubble sort algorithm below
+import random # To test with random number
+
 
 def bubble_sort(lst):
     # Iterate over the list to perform bubble sort
@@ -22,7 +24,14 @@ def bubble_sort(lst):
 
 
 # testing algorithm
-sample_list = [1, 5, 2, 6, 7]
-print(f"Unsorted list: {sample_list}")
-bubble_sort(sample_list)
-print(f"Sorted list: {sample_list}")
+# sample_list = [1, 5, 2, 601, 7, 57, 16, 87, 99, 27, 62, 4]
+# print(f"Unsorted list: {sample_list}")
+# bubble_sort(sample_list)
+# print(f"Sorted list: {sample_list}")
+
+# testing with random
+random_lst = [random.randint(-999, 999) for _ in range(10)]
+print(f"unsorted List: {random_lst}")
+bubble_sort(random_lst)
+print(f"sorted List: {random_lst}")
+
